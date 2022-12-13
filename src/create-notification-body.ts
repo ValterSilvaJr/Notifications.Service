569b1bafd53acb197b402f9c7662cef63c79d827
Valter Silva <valtersilvajr@live.com>
2022-12-12 23:39:56 -0300

    firstm commit

---
+++ src/create-notification-body.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsUUID, Length } from "class-validator";
+
+export class CreateNotificationBody {
+  @IsNotEmpty({
+    message: "O envio do recipientId é obrigatório",
+  })
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty({
+    message: "O envio do conteúdo é obrigatório",
+  })
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty({
+    message: "O envio da categoria é obrigatório",
+  })
+  category: string;
+}
